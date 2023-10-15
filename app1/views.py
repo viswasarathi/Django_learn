f8756f6836c53bfd8ac72ac6bd07249e329ca64f
viswasarathi <viswasarathi03@gmail.com>
2023-10-15 16:40:49 +0530

    Learnt about static files

--- app1/views.py
+++ app1/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt 
 # Create your views here.
 
 def index(request):
     return render(request,'home.html',{'name':'viswa'})
 
+@csrf_exempt
 def add(request):
-    x = request.GET['n1']
-    y = request.GET['n2']
+    x = request.POST['n1']
+    y = request.POST['n2']
     res = int(x) + int(y)
     return render(request,'result.html',{'result':res})
