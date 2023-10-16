20fc79e5c4f61bffea30f712d47930cd08a5150f
Rubesh <rubeshkk282gmail.com>
2023-10-16 10:34:39 +0530

    working on class and objects(Tables)

--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Destination:
+    id:int
+    place:str
+    city:str
+    price:int
+    offer : bool
--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from .models import Destination
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    dest1 = Destination()
+    dest1.place = "India"
+    dest1.city = "chennai - Marina beach"
+    dest1.price = 999
+    dest1.offer = True
+
+
+    dest2 = Destination()
+    dest2.place = "America"
+    dest2.city = "New york"
+    dest2.price = 1999
+    dest2.offer = False
+
+    dest3 = Destination()
+    dest3.place = "Japan"
+    dest3.city = "Tokyo"
+    dest3.price = 1500
+    dest3.offer = True
+
+    dests = [dest1,dest2,dest3]
+
+
+
+    return render(request,'index.html',{'dests':dests})
 
